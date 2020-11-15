b4f006b105912817e1f7d705c8d2643d0a2f945d
liuxilu <liuxilu@live.com>
2020-11-15 20:40:36 +0800

    原站

---
+++ js/general.js
@@ -0,0 +1,27 @@
+function toggle_menu_collapse() {
+   var obj = document.getElementById("navigation");
+   if (obj.className === "nav") {
+      obj.className += " responsive";
+   } else {
+      obj.className = "nav";
+   }
+}
+function submit_timeout() {
+   var inputs = document.getElementsByTagName("input");
+   for (var i = 0; i < inputs.length; i++) {
+      if (inputs[i].type === "submit" || inputs[i].type === "image") {
+         inputs[i].onclick = function() {modal.style.display = "block";return false;};
+      }
+   }
+}
+setTimeout(submit_timeout, 1000 * 3600 * 24);
+var modal = document.getElementById("modal_window");
+window.onclick = function(event) {
+   if (event.target == modal) {
+      modal.style.display = "none";
+   }
+}
+document.getElementById('hamburger').addEventListener('click', function(event) {
+   toggle_menu_collapse();
+   event.preventDefault();
+});
